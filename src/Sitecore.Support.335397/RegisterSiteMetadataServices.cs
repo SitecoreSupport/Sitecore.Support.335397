3dc99197f8dfff0778affc5081c30a492330eba4
OlegBraginskyy <obr@sitecore.net>
2019-08-27 11:21:32 +0300

    Init

---
+++ src/Sitecore.Support.335397/RegisterSiteMetadataServices.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Pipelines.IOC
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore.DependencyInjection;
+    using System;
+    using Sitecore;
+    using Sitecore.XA.Feature.SiteMetadata.Repositories.BrowserTitle;
+    using Sitecore.XA.Feature.SiteMetadata.Repositories.Favicon;
+    using Sitecore.XA.Feature.SiteMetadata.Models;
+    using Sitecore.XA.Feature.SiteMetadata.Sitemap;
+    using Sitecore.XA.Foundation.Mvc.Repositories.Base;
+    using Sitecore.XA.Feature.SiteMetadata.Repositories.SeoMetadata;
+    using Sitecore.XA.Feature.SiteMetadata.Repositories.TwitterMetadata;
+    using Sitecore.XA.Feature.SiteMetadata.Repositories.OpenGraphMetadata;
+    using Sitecore.XA.Feature.SiteMetadata.Repositories.Viewport;
+
+    public class RegisterSiteMetadataServices : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IBrowserTitleRepository, BrowserTitleRepository>();
+            serviceCollection.AddSingleton<IFaviconRepository, FaviconRepository>();
+            serviceCollection.AddSingleton<IAbstractRepository<SeoMetadataRenderingModel>, SeoMetadataRepository>();
+            serviceCollection.AddSingleton<IAbstractRepository<TwitterMetadataRenderingModel>, TwitterMetadataRepository>();
+            serviceCollection.AddSingleton<IAbstractRepository<OpenGraphRenderingModel>, OpenGraphMetadataRepository>();
+            serviceCollection.AddTransient<ISitemapGenerator, Sitecore.Support.XA.Feature.SiteMetadata.Sitemap.SitemapGenerator>();
+            serviceCollection.AddSingleton<IAbstractRepository<ViewportRenderingModel>, ViewportRepository>();
+        }
+    }
+}
---
+++ src/Sitecore.Support.335397/SitemapGenerator.cs
@@ -0,0 +1,79 @@
+﻿namespace Sitecore.Support.XA.Feature.SiteMetadata.Sitemap
+{
+    using Sitecore.Data.Items;
+    using Sitecore.Links;
+    using Sitecore.XA.Feature.SiteMetadata;
+    using Sitecore.XA.Feature.SiteMetadata.Enums;
+    using Sitecore.XA.Feature.SiteMetadata.Sitemap;
+    using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Linq;
+
+    public class SitemapGenerator : Sitecore.XA.Feature.SiteMetadata.Sitemap.SitemapGenerator
+    {
+        public SitemapGenerator()
+        {
+        }
+
+        public SitemapGenerator(XmlWriterSettings xmlWriterSettings) : base(xmlWriterSettings)
+        {
+        }
+
+        protected override StringBuilder BuildMultilanguageSitemap(IEnumerable<Item> childrenTree, SitemapLinkOptions options)
+        {
+            var urlOptions = GetUrlOptions();
+            var sitemapLinkOptions = new SitemapLinkOptions(options.Scheme, urlOptions, options.TargetHostname);
+
+            //Alternate URL should always have language embedded
+            var alternateUrlOptions = (UrlOptions)urlOptions.Clone();
+            alternateUrlOptions.LanguageEmbedding = LanguageEmbedding.Always;
+            var sitemapLinkOptionsForAlternateLinks = new SitemapLinkOptions(options.Scheme, alternateUrlOptions, options.TargetHostname);
+
+            var pages = new List<XElement>();
+            foreach (var item in childrenTree)
+            {
+                var changeFreq = item.Fields[Templates.Sitemap._Sitemap.Fields.ChangeFrequency].ToEnum<SitemapChangeFrequency>();
+                if (changeFreq == SitemapChangeFrequency.DoNotInclude)
+                {
+                    continue;
+                }
+                List<XElement> alternateUrls = new List<XElement>();
+                foreach (var itemLanguage in item.Languages)
+                {
+                    var itemAlternate = item.Database.GetItem(item.ID, itemLanguage);
+                    if (itemAlternate.Versions.Count > 0)
+                    {
+                        sitemapLinkOptionsForAlternateLinks.UrlOptions.Language = itemLanguage;
+
+                        var href = GetFullLink(itemAlternate, sitemapLinkOptionsForAlternateLinks);
+                        var hreflang = itemLanguage.CultureInfo.Name;
+                        var alternate = BuildAlternateLinkElement(href, hreflang);
+                        alternateUrls.Add(alternate);
+                    }
+                }                
+
+                sitemapLinkOptions.UrlOptions.Language = item.Language;
+                var loc = GetFullLink(item, sitemapLinkOptions);
+                var lastmod = GetUpdatedDate(item);
+                var changefreq = changeFreq.ToString().ToLowerInvariant();
+                var priority = GetPriority(item);
+                var page = BuildPageElement(loc, lastmod, changefreq, priority, alternateUrls);
+                pages.Add(page);
+
+            }
+            var document = BuildXmlDocument(pages);
+            StringBuilder builder = new StringBuilder();
+            using (TextWriter writer = new StringWriter(builder))
+            {
+                document.Save(writer);
+            }
+            FixDeclaration(builder);
+            return builder;
+        }
+    }
+}
